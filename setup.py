ec5acb10be283d22e33122739ba3dcb827814e02
Marcell Vazquez-Chanlatte <mvc@linux.com>
2018-07-20 17:33:23 -0700

    start aigbv project

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+DESC = 'A python library for manipulating sequential and-inverter gates.'
+
+setup(
+    name='py-aiger-bv',
+    version='0.0.1',
+    description=DESC,
+    url='http://github.com/mvcisback/py-aiger-bv',
+    author='Marcell Vazquez-Chanlatte',
+    author_email='marcell.vc@eecs.berkeley.edu',
+    license='MIT',
+    install_requires=[
+        'funcy',
+        'lenses',
+        'aiger',
+    ],
+    packages=find_packages(),
+)
