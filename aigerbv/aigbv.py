33d61b774a0962d0c348cc4842b416ab29c50247
Marcell Vazquez-Chanlatte <mvc@linux.com>
2018-07-24 23:50:47 -0700

    flesh out common library gates

--- aigerbv/aigbv.py
+++ aigerbv/aigbv.py
@@ -1,23 +1,21 @@
-from typing import Tuple, FrozenSet, NamedTuple, Union, Mapping, List
+from typing import Tuple, FrozenSet, NamedTuple
 
 import aiger
 import funcy as fn
-
 
 BV_MAP = FrozenSet[Tuple[str, Tuple[str]]]
 
 
 def _blast(bvname2vals, name_map):
-    return fn.merge(
-        *(dict(zip(names, bvname2vals[bvname])) for bvname, names in name_map)
-    )
+    return fn.merge(*(dict(zip(names, bvname2vals[bvname]))
+                      for bvname, names in name_map))
 
 
 def _unblast(name2vals, name_map):
     def _collect(names):
         return tuple(name2vals[n] for n in names)
 
-    return {bvname: collect(names) for bvname, names in name_map}
+    return {bvname: _collect(names) for bvname, names in name_map}
 
 
 class AIGBV(NamedTuple):
@@ -25,6 +23,36 @@ class AIGBV(NamedTuple):
     input_map: BV_MAP
     output_map: BV_MAP
     latch_map: BV_MAP
+
+    @property
+    def inputs(self):
+        return set(fn.pluck(0, self.input_map))
+
+    @property
+    def outputs(self):
+        return set(fn.pluck(0, self.output_map))
+
+    @property
+    def latches(self):
+        return set(fn.pluck(0, self.latch_map))
+
+    def __getitem__(self, others):
+        if not isinstance(others, tuple):
+            return super().__getitem__(others)
+
+        kind, relabels = others
+        if kind not in {'i', 'o', 'l'}:
+            raise NotImplementedError
+
+        attr_name = {
+            'i': 'input_map',
+            'o': 'output_map',
+            'l': 'latch_map',
+        }.get(kind)
+
+        attr_value = fn.walk_keys(lambda x: relabels.get(x, x),
+                                  getattr(self, attr_name))
+        return self._replace(**{attr_name: attr_value})
 
     def __rshift__(self, other):
         interface = self.outputs & other.inputs
@@ -50,35 +78,43 @@ class AIGBV(NamedTuple):
             aig=self.aig | other.aig,
             input_map=self.input_map | other.input_map,
             output_map=self.output_map | other.output_map,
-            latch_map=self.latch_map | other.latch_map
-        )
+            latch_map=self.latch_map | other.latch_map)
 
     def __call__(self, inputs, latches=None):
         if latches is None:
             latches = dict()
-        
+
         out_vals, latch_vals = self.aig(
             inputs=_blast(inputs, self.input_map),
-            latches=_blast(latches, self.latch_map)
-        )
+            latches=_blast(latches, self.latch_map))
         outputs = _unblast(out_vals, self.output_map)
         latch_outs = _unblast(latch_vals, self.latch_map)
         return outputs, latch_outs
 
-    # def __getitem__ <- TODO: implement renaming.
-    # def unroll <- TODO
-    # def feedback <- TODO
-    # def write <- TODO
-    # def simulator
+    def simulator(self, latches=None):
+        inputs = yield
+        while True:
+            outputs, latches = self(inputs, latches)
+            inputs = yield outputs, latches
 
-    @property
-    def inputs(self):
-        return set(fn.pluck(0, self.input_map))
+    def write(self, path):
+        self.aig.write(path)
 
-    @property
-    def outputs(self):
-        return set(fn.pluck(0, self.output_map))
+    def feedback(self):
+        raise NotImplementedError
 
-    @property
-    def latches(self):
-        return set(fn.pluck(0, self.latch_map))
+    def unroll(self):
+        raise NotImplementedError
+
+
+def _diagonal_map(keys):
+    return {k: k for k in keys}
+
+
+def aig2aigbv(aig):
+    return AIGBV(
+        aig=aig,
+        input_map=_diagonal_map(aig.inputs),
+        output_map=_diagonal_map(aig.outputs),
+        latch_map=_diagonal_map(aig.latches),
+    )
--- aigerbv/common.py
+++ aigerbv/common.py
@@ -1,10 +1,14 @@
 import operator as op
 from functools import reduce
+from uuid import uuid1
 
 import aiger
-import funcy as fn
 
 from aigerbv import aigbv
+
+
+def _fresh():
+    return str(uuid1())
 
 
 def _named_indexes(wordlen, root):
@@ -16,10 +20,8 @@ def bitwise_binop(binop, wordlen, left='x', right='y', output='x&y'):
     rights = _named_indexes(wordlen, right)
     outputs = _named_indexes(wordlen, output)
 
-    aig = reduce(
-        op.or_,
-        (binop([l, r], o) for l, r, o in zip(lefts, rights, outputs))
-    )
+    aig = reduce(op.or_, (binop([l, r], o)
+                          for l, r, o in zip(lefts, rights, outputs)))
     return aigbv.AIGBV(
         aig=aig,
         input_map=frozenset([(left, lefts), (right, rights)]),
@@ -82,22 +84,85 @@ def source(wordlen, value, name='x', signed=True, byteorder='little'):
     bits = value.to_bytes(wordlen, byteorder, signed=signed)
     names = _named_indexes(wordlen, name)
     return aigbv.AIGBV(
-        aig=aiger.source({name: bit for name, bit in zip(names, bits)}),
-        input_map=frozenset([(name, names)]),
+        aig=aiger.source({name: bit
+                          for name, bit in zip(names, bits)}),
+        input_map=frozenset(),
         output_map=frozenset([(name, names)]),
         latch_map=frozenset(),
     )
 
 
+def identity_gate(wordlen, input='x', output='x'):
+    return aigbv.aig2aigbv(
+        aiger.identity(
+            inputs=_named_indexes(wordlen, input),
+            outputs=_named_indexes(output)))
+
+
+def reverse_gate(wordlen, input='x', output='rev(x)'):
+    circ = identity_gate(wordlen, input, output)
+    return identity_gate(wordlen, input, output)._replace(
+        output_map=frozenset((k, reversed(vs)) for k, vs in circ.output_map))
+
+
+def combine_gate(left_wordlen, left, right_wordlen, right, output):
+    lefts = _named_indexes(left_wordlen, left)
+    rights = _named_indexes(right_wordlen, right)
+    outputs = _named_indexes(left_wordlen + right_wordlen, output)
+
+    aig = identity_gate(left_wordlen, left, left) \
+        | identity_gate(right_wordlen, right, right)
+    return aig >> aigbv.aig2aigbv(
+        aiger.identity(inputs=lefts + rights, outputs=outputs))
+
+
+def split_gate(left_wordlen, left, right_wordlen, right, input):
+    inputs = _named_indexes(left_wordlen + right_wordlen, input)
+    lefts = _named_indexes(left_wordlen, left)
+    rights = _named_indexes(right_wordlen, right)
+
+    aig = identity_gate(left_wordlen, left, left) \
+        | identity_gate(right_wordlen, right, right)
+    return aigbv.aig2aigbv(
+        aiger.identity(inputs=inputs, outputs=lefts + rights)) >> aig
+
+
+def unsigned_right_shift_gate(wordlen, shift, input='x', output='x'):
+    assert 0 <= shift < wordlen
+
+    if shift == 0:
+        return identity_gate(wordlen, input, output)
+
+    inputs = _named_indexes(wordlen, input)
+    outputs = _named_indexes(wordlen, output)
+    _, shifted_i = inputs[:shift], inputs[shift:]
+    const_o, shifted_o = outputs[:shift], outputs[shift:]
+
+    aig = aiger.identity(shifted_i, shifted_o)
+    aig |= aiger.source({n: False for n in const_o})
+    return aigbv.AIGBV(
+        aig=aig,
+        input_map=frozenset([(input, inputs)]),
+        output_map=frozenset([output, outputs]),
+        latch_map=frozenset(),
+    )
+
+
+def unsigned_left_shift_gate(wordlen, shift, input='x', output='x'):
+    return reverse_gate(wordlen, input, 'tmp') \
+        >> unsigned_right_shift_gate(wordlen, shift, 'tmp', 'tmp') \
+        >> reverse_gate(wordlen, 'tmp', output)
+
+
 def _full_adder(x, y, carry_in, result, carry_out):
-    # TODO: Rewrite in aiger. 
+    # TODO: Rewrite in aiger.
     return aiger.parse(
         "aag 10 3 0 2 7\n2\n4\n6\n18\n21\n8 4 2\n10 5 3\n"
         "12 11 9\n14 12 6\n16 13 7\n18 17 15\n20 15 9\n"
         f"i0 {x}\ni1 {y}\ni2 {carry_in}\no0 {result}\no1 {carry_out}\n")
 
 
-def add_gate(wordlen, output='x+y', left='x', right='y', has_carry=False):
+def add_gate(wordlen, left='x', right='y', output='x+y', has_carry=False):
     carry_name = f'{output}_carry'
     assert left != carry_name and right != carry_name
 
@@ -113,12 +178,11 @@ def add_gate(wordlen, output='x+y', left='x', right='y', has_carry=False):
             y=rname,
             carry_in=carry_name,
             result=oname,
-            carry_out=carry_name
-        )
+            carry_out=carry_name)
 
     if not has_carry:
         adder_aig >>= aiger.sink([output + '_carry'])
-    
+
     return aigbv.AIGBV(
         aig=adder_aig,
         input_map=frozenset([(left, lefts), (right, rights)]),
@@ -127,45 +191,28 @@ def add_gate(wordlen, output='x+y', left='x', right='y', has_carry=False):
     )
 
 
-def leq_gate():
+def negate_gate(wordlen, input='x', output='~x'):
+    """Implements two's complement negation."""
+    inc = source(wordlen, 1, 'const1') \
+        >> add_gate(wordlen, 'tmp', 'const1', output)
+    return bitwise_negate(wordlen, input, 'tmp') >> inc
+
+
+def subtract_gate(wordlen, left='x', right='y', output='x-y'):
+    tmp = _fresh()
+    return negate_gate(wordlen, right, tmp) \
+        >> add_gate(wordlen, left, tmp, output)
+
+
+def signed_lt_gate(wordlen, left, right, output):
     raise NotImplementedError
 
 
-def geq_gate():
-    raise NotImplementedError
-
-
-def lt_gate():
-    raise NotImplementedError
-
-
-def gt_gate():
-    raise NotImplementedError
+def signed_le_gate(wordlen, left, right, output):
+    tmp = _fresh()
+    return signed_lt_gate(wordlen, right, left, tmp) \
+        >> bitwise_negate(1, tmp, output)
 
 
 def abs_gate():
-    raise NotImplementedError
-
-
-def concat_gate(wordlen, input='x', output='rev(x)'):
-    raise NotImplementedError
-
-
-def repeat_gate(wordlen, input='x', output='rev(x)'):
-    raise NotImplementedError
-
-
-def reverse_gate(wordlen, input='x', output='rev(x)'):
-    raise NotImplementedError
-
-
-def negate_gate(wordlen, input='x', output='~x'):
-    raise NotImplementedError
-
-
-def subtract_gate(wordlen, output='x-y', left='x', right='y'):
-    raise NotImplementedError
-
-
-def shift_gate(wordlen, shift, input='x', output='x', signed=True):
     raise NotImplementedError
